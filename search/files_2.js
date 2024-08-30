17bf04c67e8b46577370e5d60d0989ada114bd4d
KSkwarczynski <KSkwarczynski@users.noreply.github.com>
2024-08-30 16:40:56 +0000

    deploy: 60f4412c991c4a5c17cfc0ac679667dab81254cf

--- search/files_2.js
+++ search/files_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['diagmcmc_2ecpp_0',['DiagMCMC.cpp',['../DiagMCMC_8cpp.html',1,'']]]
+  ['diagmcmc_2ecpp_0',['DiagMCMC.cpp',['../DiagMCMC_8cpp.html',1,'']]],
+  ['diagnostics_2freadme_2emd_1',['README.md',['../Diagnostics_2README_8md.html',1,'']]]
 ];
